d733a457d8cf9f952da98e27b1d19fcdf316828f
OritLeshem <azmono@gmail.com>
2022-12-17 08:06:33 -0500

    search

--- js/meme/meme.service.js
+++ js/meme/meme.service.js
@@ -5,101 +5,114 @@ var gImgs
 var gMeme
 var gSelectedLineIdx
 
-gImgs = [
-  {
-    id: 1,
-    url: 'meme-imgs/1.jpg',
-    keywords: ['trump']
-  },
-  {
-    id: 2,
-    url: 'meme-imgs/2.jpg',
-    keywords: ['animal', 'dog']
-  },
-  {
-    id: 3,
-    url: 'meme-imgs/3.jpg',
-    keywords: ['baby']
-  },
-  {
-    id: 4,
-    url: 'meme-imgs/4.jpg',
-    keywords: ['animal', 'cat']
-  },
-  {
-    id: 5,
-    url: 'meme-imgs/5.jpg',
-    keywords: ['baby']
-  },
-  {
-    id: 6,
-    url: 'meme-imgs/6.jpg',
-    keywords: []
-  },
-  {
-    id: 7,
-    url: 'meme-imgs/7.jpg',
-    keywords: ['baby']
-  },
-  {
-    id: 8,
-    url: 'meme-imgs/8.jpg',
-    keywords: []
-  },
-  {
-    id: 9,
-    url: 'meme-imgs/9.jpg',
-    keywords: ['baby']
-  },
-  {
-    id: 10,
-    url: 'meme-imgs/10.jpg',
-    keywords: ['obama']
-  },
-  {
-    id: 11,
-    url: 'meme-imgs/11.jpg',
-    keywords: []
-  },
-  {
-    id: 12,
-    url: 'meme-imgs/12.jpg',
-    keywords: []
-  },
-  {
-    id: 13,
-    url: 'meme-imgs/13.jpg',
-    keywords: ['leonardo', 'dicaprio',]
-  },
-  {
-    id: 14,
-    url: 'meme-imgs/14.jpg',
-    keywords: []
-  },
-  {
-    id: 15,
-    url: 'meme-imgs/15.jpg',
-    keywords: []
-  },
-  {
-    id: 16,
-    url: 'meme-imgs/16.jpg',
-    keywords: []
-  },
-  {
-    id: 17,
-    url: 'meme-imgs/17.jpg',
-    keywords: ['putin']
-  },
-  {
-    id: 18,
-    url: 'meme-imgs/18.jpg',
-    keywords: ['toy']
-  }
+
+var gImgs = [{
+  id: 1,
+  url: 'meme-imgs/1.jpg',
+  keywords: ['trump', 'akward']
+},
+{
+  id: 2,
+  url: 'meme-imgs/2.jpg',
+  keywords: ['fun', 'dog']
+}, {
+  id: 3,
+  url: 'meme-imgs/3.jpg',
+  keywords: ['baby', 'dog']
+},
+{
+  id: 4,
+  url: 'meme-imgs/4.jpg',
+  keywords: ['cat', 'sleeping']
+},
+{
+  id: 5,
+  url: 'meme-imgs/5.jpg',
+  keywords: ['boy', 'succses']
+},
+{
+  id: 6,
+  url: 'meme-imgs/6.jpg',
+  keywords: ['funny', 'smiling']
+},
+{
+  id: 7,
+  url: 'meme-imgs/7.jpg',
+  keywords: ['baby', 'smiling']
+},
+{
+  id: 8,
+  url: 'meme-imgs/8.jpg',
+  keywords: ['guy', 'smiling']
+},
+{
+  id: 9,
+  url: 'meme-imgs/9.jpg',
+  keywords: ['boy', 'smiling']
+},
+{
+  id: 10,
+  url: 'meme-imgs/10.jpg',
+  keywords: ['obama', 'smiling']
+},
+{
+  id: 11,
+  url: 'meme-imgs/11.jpg',
+  keywords: ['angry']
+},
+{
+  id: 12,
+  url: 'meme-imgs/12.jpg',
+  keywords: ['wright']
+},
+{
+  id: 13,
+  url: 'meme-imgs/13.jpg',
+  keywords: ['smiling', 'leonardo']
+},
+{
+  id: 14,
+  url: 'meme-imgs/14.jpg',
+  keywords: ['metrix', 'sunglasses']
+},
+{
+  id: 15,
+  url: 'meme-imgs/15.jpg',
+  keywords: ['men', 'smiling']
+},
+{
+  id: 16,
+  url: 'meme-imgs/16.jpg',
+  keywords: ['starwars', 'smiling']
+},
+{
+  id: 17,
+  url: 'meme-imgs/17.jpg',
+  keywords: ['putin']
+},
+{
+  id: 18,
+  url: 'meme-imgs/18.jpg',
+  keywords: ['toy']
+},
 ]
 
+
 function getImages() {
-  return gImgs
+  var elSearch = document.querySelector('.search')
+  var val = elSearch.value
+  console.log(val)
+  var imgForFilter = gImgs
+  var gFilter = []
+  for (var i = 0; i < imgForFilter.length; i++) {
+    var res = imgForFilter[i].keywords.filter(word => word.includes(val))
+    if (res.length > 0) gFilter.push(imgForFilter[i])
+    console.log(res, res.length, i)
+  }
+  console.log(gFilter)
+  if (gFilter.length > 0) return gFilter
+  else return imgForFilter
+
 }
 function createMeme(id, url) {
   gMeme = {
--- js/test.js
+++ js/test.js
@@ -1,12 +1,133 @@
 console.log('hello form tst')
-var ggImg = [{
-  id: 1,
-  url: 'meme-imgs/1.jpg',
-  keyword: ['trump', 'akward']
-},
-{
-  id: 2,
-  url: 'meme-imgs/2.jpg',
-  keyword: ['baby', 'dog']
-}]
-console.log(ggImg)
+var gWordsCounter = []
+// var ggImg = [{
+//   id: 1,
+//   url: 'meme-imgs/1.jpg',
+//   keywords: ['trump', 'akward']
+// },
+// {
+//   id: 2,
+//   url: 'meme-imgs/2.jpg',
+//   keywords: ['fun', 'dog']
+// }, {
+//   id: 3,
+//   url: 'meme-imgs/3.jpg',
+//   keywords: ['baby', 'dog']
+// },
+// {
+//   id: 4,
+//   url: 'meme-imgs/4.jpg',
+//   keywords: ['cat', 'sleeping']
+// },
+// {
+//   id: 5,
+//   url: 'meme-imgs/5.jpg',
+//   keywords: ['boy', 'succses']
+// },
+// {
+//   id: 6,
+//   url: 'meme-imgs/6.jpg',
+//   keywords: ['funny', 'smiling']
+// },
+// {
+//   id: 7,
+//   url: 'meme-imgs/7.jpg',
+//   keywords: ['baby', 'smiling']
+// },
+// {
+//   id: 8,
+//   url: 'meme-imgs/8.jpg',
+//   keywords: ['guy', 'smiling']
+// },
+// {
+//   id: 9,
+//   url: 'meme-imgs/9.jpg',
+//   keywords: ['boy', 'smiling']
+// },
+// {
+//   id: 10,
+//   url: 'meme-imgs/10.jpg',
+//   keywords: ['obama', 'smiling']
+// },
+// {
+//   id: 11,
+//   url: 'meme-imgs/11.jpg',
+//   keywords: ['angry']
+// },
+// {
+//   id: 12,
+//   url: 'meme-imgs/12.jpg',
+//   keywords: ['wright']
+// },
+// {
+//   id: 13,
+//   url: 'meme-imgs/13.jpg',
+//   keywords: ['smiling', 'leonardo']
+// },
+// {
+//   id: 14,
+//   url: 'meme-imgs/14.jpg',
+//   keywords: ['metrix', 'sunglasses']
+// },
+// {
+//   id: 15,
+//   url: 'meme-imgs/15.jpg',
+//   keywords: ['men', 'smiling']
+// },
+// {
+//   id: 16,
+//   url: 'meme-imgs/16.jpg',
+//   keywords: ['starwars', 'smiling']
+// },
+// {
+//   id: 17,
+//   url: 'meme-imgs/17.jpg',
+//   keywords: ['putin']
+// },
+// {
+//   id: 18,
+//   url: 'meme-imgs/18.jpg',
+//   keywords: ['toy']
+// },
+// ]
+var wordsCountMap = []
+gImgs.forEach(el => el.keywords.forEach(word => gWordsCounter.push(word)))
+
+for (var i = 0; i < gWordsCounter.length; i++) {
+  var currWord = gWordsCounter[i]
+  wordsCountMap[currWord] = wordsCountMap[currWord] ? ++wordsCountMap[currWord] : 1
+}
+console.log(wordsCountMap)
+var words = wordsCountMap.slice()
+console.log(words)
+
+renderSearch()
+function renderSearch() {
+
+}
+wordsCountMap
+console.log(wordsCountMap.length)
+console.log('hellp')
+for (var i = 0; i < wordsCountMap.length; i++) {
+  console.log('he')
+}
+
+// var strHTML = words.map(word => word)
+// console.log(wordsCountMap)
+
+// var arr1 = ['woe', 'ff', 'gg']
+// var arr2 = wordsCountMap.map(el => el)
+// console.log(arr2)
+// var strHTML = words.map((word) => {
+// words.map((word) => {
+//   // return `<div> ${word} ></div>`
+//   console.log(word)
+// return `<div> hello ></div>`
+// return `<div onclick="onWord(${word[key]},'${img.url}')"><img  class="img-in-gallery"src="${img.url}"></div>`
+// })
+// .join('')
+// document.querySelector('.word-box').innerHTML = strHTML
+// }
+
+
+
